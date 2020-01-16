c66d3405617a88e773e8fd46242c4d3f80fa369c
rupuka <rupuka@hotmail.com>
2020-01-15 21:46:28 -0600

    Update Lista.h

--- Lista.h
+++ Lista.h
@@ -7,23 +7,31 @@ protected:
 int Max=50;
 int lista[Max];
 int x;
-
+int index;
 
 public:
   
 int recurperar_sucesor(){
 //recuperar sucesor
-cin >> x;
-if (x+1 > index) {
-  cout << lista[x+1];
+if(Lista == 0){
+  cout << "lista vacia";
+}else {
+  cin >> x;
+  if (x+1 > index) {
+   cout << lista[x+1];
   }
 }
+  
 int recuperar_elemento(){
 //recuperar un elemento
-cin >> x;
-if (x > index) {
+if(Lista == 0){
+  cout << "lista vacia";
+  }else{
+ cin >> x;
+ if (x > index) {
   cout << lista[x];
-}
+  }
 }
 
 }
+
