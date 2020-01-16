e877a58a4593d80304de00f04c8e0daad2a85673
yael <yael.atletl@gmail.com>
2020-01-15 21:40:11 -0600

    convertir a OOP

--- Lista.h
+++ Lista.h
@@ -1,16 +1,29 @@
 /* Lista.h*/
 #include <stdio.h>
+
+class Lista{
+protected:
+
 int Max=50;
 int lista[Max];
-int x
+int x;
 
+
+public:
+  
+int recurperar_sucesor(){
 //recuperar sucesor
 cin >> x;
 if (x+1 > index) {
   cout << lista[x+1];
+  }
 }
+int recuperar_elemento(){
 //recuperar un elemento
 cin >> x;
 if (x > index) {
   cout << lista[x];
+}
+}
+
 }
