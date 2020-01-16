8c82e0971db66235c9eccc9a6d5dfb67ba922aaf
angelsc9 <45831938+angelsc9@users.noreply.github.com>
2020-01-15 21:18:14 -0600

    Update main.cpp
    se ve algo

--- main.cpp
+++ main.cpp
@@ -2,6 +2,24 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+class Lista{
+	private :
+		#difine Max 50;
+		int array[Max];
+		
+	public:
+		Lista(int arreglo); //constructor 
+		void insercion(int arreglo, int elemento);
+		void suprimir(int arreglo );
+		void eliminar_los_elementos_duplicados();
+		int recuperar_un_elemento(int elemento);
+		int recuperar_sucesor();
+		int recuperar_predecesor();
+};
+
 int main(int argc, char** argv) {
 	return 0;
+	
+	//dentro de qui haremos todas las funciones ?
+	
 }
