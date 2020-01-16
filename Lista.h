cd3b80ab4ab3275674b44b257a1beb547556e866
rupuka <rupuka@hotmail.com>
2020-01-15 21:28:31 -0600

    Update Lista.h

--- Lista.h
+++ Lista.h
@@ -1,2 +1,15 @@
 /* Lista.h*/
 #include <math.h>
+
+int x
+
+//recuperar sucesor
+cin >> x;
+if (x+1 > index) {
+  cout << lista[x+1];
+  
+//recuperar un elemento
+cin >> x;
+if (x > index) {
+  cout << lista[x];
+} 
