7124cfd1125eeb85afb8c93fc550c322d740ba71
rupuka <rupuka@hotmail.com>
2020-02-22 23:39:38 -0600

    Update derivados.h

--- listas_dinamicas/derivados.h
+++ listas_dinamicas/derivados.h
@@ -1,3 +1,4 @@
+//Manejo de librerias
 #ifdef S 
 #include "Lista_ligada.h"
 #endif
@@ -11,21 +12,21 @@
 #include "Lista_doble_ligada_circular.h"
 #endif
 #endif 
-
+//Manejo de excepciones pila
 class Pila: public Lista{
 public:
   void push(int x, int* bandera){
     insertar_final(x, bandera);
     
-  }
+  }//Manejo de excepcion suprimir
   int pop(int* bandera){
     return suprimir_ultimo(bandera);
     if (*bandera!=OK) printf("No se pudo hacer pop\n");
-  }
+  }//Manejo de excepcion recuperar ultimo
   int ultimo(int* bandera){
     return recuperar_ultimo(bandera);
     if (*bandera!=OK) printf("No se pudo recuperar ultimo\n");
-  }
+  }//Manejo de estado
   void estado(int* bandera){
       if(lista_vacia(bandera)){
           printf("La Pila esta vacia \n");
@@ -36,21 +37,21 @@ public:
 };
 
 
-
+//Manejo de excepciones cola
 class Cola: public Lista{
 public:
   void encolar(int x, int* bandera){
     insertar_final(x, bandera);
   }
-
+//Manejo de excepcion desencolar
   int desencolar(int* bandera){
     return suprimir_primero(bandera);
     if (*bandera!=OK) printf("No se puede deseconlar\n");
-  }
+  }//Manejo de excepcion recupear posicion de elemento
   int recuperar(int x, int* bandera){
     return recuperar_pos_elemento(x, bandera);
     if (*bandera!=OK) printf("No se puede recuperar la posicion del elemento %d\n", x);
-  }
+  }//Manejo de estado
   void estado(int* bandera){
       if(lista_vacia(bandera)){
           printf("La Cola esta vacia \n");
