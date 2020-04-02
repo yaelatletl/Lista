2e601f36ef72835517f8b6ba7ca431705f635fa0
yael <yael.atletl@gmail.com>
2020-04-02 14:55:20 -0600

    Arbol derivado de grafo

--- grafos3/arbol.h
+++ grafos3/arbol.h
@@ -10,24 +10,36 @@ class Arbol: public Lista{
 
 
     int calcular_peso(){
-return 1;
-}
+      return 1;
+    }
+    Arbol *obtener_hijo(int pos, int *bandera){
+      return (Arbol*)recuperar_elemento(pos, bandera);
+    }
+
 
 };
 
-class ArbolBinario: public Arbol{
+class ArbolBinario: public NodoGrafo{
 public:
   bool esta_lleno();
   void posorden(Arbol nodo){
     if (nodo==NULL) return;
-    posorden(nodo.hijo1);
-    posorden(nodo.hijo2);
+    posorden(*(nodo.obtener_hijo(0,bandera)); //hijo1
+    posorden(*(nodo.obtener_hijo(1, bandera))); //hijo2
     printf("Nodo %c\n", nodo.id);
   }
   void preorden(Arbol nodo){
     if (nodo==NULL) return;
     printf("Nodo %c\n", nodo.id);
-    preorden(nodo.hijo1);
-    preorden(nodo.hijo2);
+    preorden(*(nodo.obtener_hijo(0, bandera))); //hijo1
+    preorden(*(nodo.obtener_hijo(1, bandera))); //hijo2
+
+  void inorden(Arbol nodo){
+    if (nodo==null) return;
+    preorden(*(nodo.obtener_hijo(0, bandera))); //hijo1
+    printf("Nodo %c\n", nodo.id);
+    preorden(*(nodo.obtener_hijo(1, bandera))); //hijo2
+  }
+
   }
 }
